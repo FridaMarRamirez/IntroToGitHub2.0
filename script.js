950e8aaaa65fbc12e317da9ad6971e5254fe513a
bporto1313 <165727791+bporto1313@users.noreply.github.com>
2024-04-01 20:20:28 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+// Simple JavaScript script for demonstration purposes
+document.addEventListener("DOMContentLoaded", function() {
+    // Change background color of the header on click
+    document.querySelector('header').addEventListener('click', function() {
+        this.style.backgroundColor = 'crimson';
+    });
+});
